30d1a9a9379edf615119d6c79526b5d7d5fdbd6d
Joel Sunny <joel.sunny@experionglobal.com>
2022-11-30 15:38:07 +0530

    Modified App Componenet and Add Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'login', component: LoginComponent},
+  { path : 'register', component : RegisterComponent },
+  { path : '', redirectTo : 'register', pathMatch : 'full' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

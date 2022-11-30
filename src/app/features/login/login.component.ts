db650f67d17e8e41d5d7b9a21e55e90a25ee646a
Joel Sunny <joel.sunny@experionglobal.com>
2022-11-30 15:40:36 +0530

    Added Login Component

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserLogin } from 'src/app/shared/models/user-login';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  
+  loginForm!: FormGroup;
 
-  constructor() { }
+  user = new UserLogin();
+  
+  constructor(private authService : AuthService, private fb : FormBuilder) { }
 
-  ngOnInit(): void {
+  login(user : UserLogin) {
+
+    user.username = this.loginForm.controls['username'].value;
+    user.password = this.loginForm.controls['password'].value;
+
+    this.authService.login(user).subscribe((token : string) => {
+      localStorage.setItem('authToken', token);
+    })
+  }
+
+  ngOnInit() : void {
+    this.loginForm = this.fb.group({
+      username : new FormControl(''),
+      password : new FormControl('')
+
+    });
+
+    this.loginForm.valueChanges.subscribe(console.log)
   }
 
 }

a00baed71a61dd24db6028c3d92a168a8d4177ac
Joel Sunny <joel.sunny@experionglobal.com>
2022-11-30 15:47:19 +0530

    Added Register Componenet

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { UserRegister } from 'src/app/shared/models/user-register';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm! : FormGroup;
+
+  user = new UserRegister();
+
+  constructor(private authService : AuthService, private fb : FormBuilder) { }
+
+  register(user : UserRegister) {
+
+    user.firstName = this.registerForm.controls['firstName'].value;
+    user.lastName = this.registerForm.controls['lastName'].value;
+    user.role = Number(this.registerForm.controls['role'].value);
+    user.username = this.registerForm.controls['username'].value;
+    user.password = this.registerForm.controls['password'].value;
+
+    this.authService.register(user).subscribe();
+  }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+
+      firstName : new FormControl(''),
+      lastName : new FormControl(''),
+      role : new FormControl(''),
+      username : new FormControl(''),
+      password : new FormControl('')
+    })
   }
 
 }

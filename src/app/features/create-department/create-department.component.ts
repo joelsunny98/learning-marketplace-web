15dbc7b5bd62499ad21306a516b6f0c5791f3456
Joel Sunny <joel.sunny@experionglobal.com>
2022-12-07 12:34:56 +0530

    Added create course component

---
+++ src/app/features/create-department/create-department.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-department',
+  templateUrl: './create-department.component.html',
+  styleUrls: ['./create-department.component.scss']
+})
+export class CreateDepartmentComponent {
+
+}
